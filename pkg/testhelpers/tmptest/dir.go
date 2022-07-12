ba3b688c22aeb43ba3e6212f38a0654d19e4501d
Sam Salisbury <samsalisbury@gmail.com>
2022-07-12 13:20:30 +0100

    add options to testtmp.Dir

--- pkg/testhelpers/tmptest/dir.go
+++ pkg/testhelpers/tmptest/dir.go
@@ -4,13 +4,39 @@ import (
 	"os"
 	"strings"
 	"testing"
+
+	cp "github.com/otiai10/copy"
 )
 
-func Dir(t *testing.T) string {
+type Settings struct {
+	contentsFrom    string
+	topLevelTempDir string
+}
+
+type Option func(*Settings)
+
+func WithContentsOf(dir string) Option {
+	return func(s *Settings) { s.contentsFrom = dir }
+}
+
+func WithTopLevelTempDir(dir string) Option {
+	return func(s *Settings) { s.topLevelTempDir = dir }
+}
+
+func Dir(t *testing.T, opts ...Option) string {
 	t.Helper()
+	s := &Settings{}
+	for _, o := range opts {
+		o(s)
+	}
 	name := strings.ReplaceAll(t.Name(), "/", "_")
-	f, err := os.MkdirTemp("", name+".*")
+	dir, err := os.MkdirTemp(s.topLevelTempDir, name+".*")
 	must(t, err)
-	must(t, os.Chmod(f, os.ModePerm))
-	return f
+	must(t, os.Chmod(dir, os.ModePerm))
+
+	if s.contentsFrom != "" {
+		must(t, cp.Copy(s.contentsFrom, dir))
+	}
+
+	return dir
 }
