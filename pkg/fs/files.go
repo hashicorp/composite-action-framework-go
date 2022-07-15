750e3a0989b3fad4d8f4c3a614057727424307ea
Sam Salisbury <samsalisbury@gmail.com>
2022-07-15 11:27:38 +0100

    add fs.(With|Write)TempFile funcs

--- pkg/fs/files.go
+++ pkg/fs/files.go
@@ -1,6 +1,8 @@
 package fs
 
 import (
+	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -21,4 +23,28 @@ func WriteFile[T Bytes](path string, contents T) error {
 		return err
 	}
 	return ioutil.WriteFile(path, []byte(contents), os.ModePerm)
+}
+
+// WriteTempFile writes contents to a unique temporary file and returns its path.
+func WriteTempFile[T Bytes](name string, contents T) (string, error) {
+	return WithTempFile(name, func(w io.Writer) error {
+		_, err := w.Write([]byte(contents))
+		return err
+	})
+}
+
+// WithTempFile creates a unique temporary file, runs the 'do' function you provide,
+// passing the file as an io.Writer, and then closes the file. It returns the
+// file's path, or an error from 'do', or a general write or close error.
+func WithTempFile(name string, do func(io.Writer) error) (string, error) {
+	name = fmt.Sprintf("%s.*", name)
+	tempFile, err := os.CreateTemp("", name)
+	if err != nil {
+		return "", err
+	}
+	if err := do(tempFile); err != nil {
+		tempFile.Close()
+		return "", err
+	}
+	return tempFile.Name(), tempFile.Close()
 }
