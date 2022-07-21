2b4e336f3c2440ebc26d78f96b348062dfdf0b43
Sam Salisbury <samsalisbury@gmail.com>
2022-07-21 11:36:55 +0100

    github: expose step summary writer

--- pkg/github/stepsummary.go
+++ pkg/github/stepsummary.go
@@ -4,7 +4,6 @@ import (
 	"flag"
 	"fmt"
 	"io"
-	"log"
 	"os"
 )
 
@@ -17,6 +16,7 @@ type StepSummary struct {
 	flagSet  *flag.FlagSet
 	enabled  bool
 	filePath string
+	file     *os.File
 }
 
 func (gss *StepSummary) ReadEnv() error {
@@ -31,30 +31,27 @@ func (gss *StepSummary) Flags(fs *flag.FlagSet) {
 	fs.BoolVar(&gss.enabled, StepSumarryEnabledFlag, enabledByDefault, desc)
 }
 
-func (gss *StepSummary) Write(s string, echo io.Writer) error {
+func (gss *StepSummary) Open() (io.Writer, error) {
 	if !gss.enabled {
+		return nil, nil
+	}
+	if gss.filePath == "" {
+		return nil, fmt.Errorf("%s is empty", StepSummaryPathEnv)
+	}
+	var err error
+	if gss.file, err = openAppend(gss.filePath); err != nil {
+		return nil, err
+	}
+	return gss.file, nil
+}
+
+func (gss *StepSummary) Close() error {
+	if gss.file == nil {
 		return nil
 	}
-	var (
-		w          io.Writer
-		closeError error
-	)
-	if gss.filePath == "" {
-		log.Printf("warning: %s is empty, so not writing to that file.", StepSummaryPathEnv)
-		w = echo
-	} else {
-		file, err := os.OpenFile(gss.filePath, os.O_APPEND|os.O_WRONLY, os.ModePerm)
-		if err != nil {
-			return err
-		}
-		defer func() { closeError = file.Close() }()
-		log.Printf("Writing step summary to %s", gss.filePath)
-		w = io.MultiWriter(file, echo)
-	}
-	_, err := w.Write([]byte(s))
-	if err != nil {
-		return err
-	}
+	return gss.file.Close()
+}
 
-	return closeError
+func openAppend(path string) (*os.File, error) {
+	return os.OpenFile(path, os.O_APPEND|os.O_WRONLY, os.ModePerm)
 }
